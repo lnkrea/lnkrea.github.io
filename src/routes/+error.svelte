d6addc6709342031504161cc2c83c6deee1efe8b
tdvnm <shubhrogupta117@gmail.com>
2025-02-05 22:45:37 +0530

    Update +error.svelte

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,1 +1,24 @@
-<h1>what you on??</h1>
+<div>
+<h1>403 Forbidden</h1>
+<p>Sorry, you are not authorized to access this page.</p>
+</div>
+
+<style lang="scss">
+    div {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+
+        color: var(--main-dark);
+            font-size: 4em;
+        h1 {
+            margin-bottom: 0;
+        }
+        p {
+            opacity: 0.8;
+        }
+    }
+
+</style>
